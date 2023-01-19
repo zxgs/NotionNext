0ac9ae77c13f15e849b2f831eb014c2885456f4f
zxgs <65719131+zxgs@users.noreply.github.com>
2023-01-20 01:11:42 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -12,14 +12,14 @@ const BLOG = {
 
   AUTHOR: '张小馋', // 作者
   BIO: '一个不爱吃香菜的馋🍚', // 作者简介
-  LINK: 'https://tangly1024.com', // 网站地址
+  LINK: 'https://github.com/zxgs', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_EMAIL: 'mail@tangly1024.com', // 邮箱
+  CONTACT_EMAIL: 'jczxgs@gmail.com', // 邮箱
   CONTACT_WEIBO: '', // 你的微博个人主页
   CONTACT_TWITTER: '', // 你的twitter个人主页
-  CONTACT_GITHUB: 'https://github.com/tangly1024', // 你的github个人主页
-  CONTACT_TELEGRAM: 'https://t.me/tangly_1024', // 你的telegram 地址 例如 https://t.me/tangly_1024
+  CONTACT_GITHUB: 'https://github.com/zxgs', // 你的github个人主页
+  CONTACT_TELEGRAM: 'https://t.me/Small_Ch', // 你的telegram 地址 例如 https://t.me/tangly_1024
   CONTACT_LINKEDIN: '', // 你的linkedIn 首页
 
   // 网站默认使用PingFangSC及NotoSansSC，
